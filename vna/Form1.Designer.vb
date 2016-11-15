b359b3e22a767bc5f022cbfc6f9fee1d04a26ddd
Marc McClanahan <marc.m@coppermountaintech.com>
2016-11-15 11:14:32 -0500

    Add project files.

---
+++ vna/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿'Copyright ©2016 Copper Mountain Technologies
+'
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+' to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+' and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+'
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+'
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(44, 96)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(205, 59)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "VNA Automation Test"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+End Class
---
+++ vna/Form1.vb
@@ -0,0 +1,73 @@
+﻿'Copyright ©2016 Copper Mountain Technologies
+'
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+' to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+' and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+'
+' The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+'
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+' MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+' ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+' THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'Imports Instr = R54Lib
+'Imports Instr = TR1300Lib
+Imports Instr = S2VNALib
+'Imports Instr = S4VNALib
+
+Public Class Form1
+
+    'Private vna As Instr.RVNA
+    'Private vna As Instr.TRVNA
+    Private vna As Instr.S2VNA
+    'Private vna As Instr.S4VNA
+
+    Private vnaFdata(100) As Double
+    Private vnaMdata(100) As Double
+    Private Const numPoints As Integer = 7
+    Private sw As Stopwatch
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'vna = New Instr.RVNA
+        'vna = New Instr.TRVNA
+        vna = New Instr.S2VNA
+        'vna = New Instr.S4VNA
+
+        sw = New Stopwatch
+
+        sw.Reset()
+        sw.Start()
+
+        ' wait until vna has initialized
+        ' exit if initialization takes longer than 25 seconds
+        If Not vna.Ready Then
+            Do While Not vna.Ready
+                If sw.ElapsedMilliseconds >= 25000 Then
+                    MsgBox("VNA Not Responding", CType(MsgBoxStyle.Critical + MsgBoxStyle.SystemModal, MsgBoxStyle), "VNA Automation Test")
+                    sw.Stop()
+                    Exit Sub
+                End If
+            Loop
+            sw.Stop()
+            MsgBox(vna.NAME, CType(MsgBoxStyle.Information + MsgBoxStyle.SystemModal, MsgBoxStyle), "VNA Automation Test")
+        Else
+            sw.Stop()
+            MsgBox(vna.NAME, CType(MsgBoxStyle.Information + MsgBoxStyle.SystemModal, MsgBoxStyle), "VNA Automation Test")
+        End If
+
+        vna.SCPI.SYSTem.PRESet()
+        vna.SCPI.SENSe(1).FREQuency.STARt = 400000000
+        vna.SCPI.SENSe(1).FREQuency.STOP = 420000000
+        vna.SCPI.SENSe(1).SWEep.POINts = numPoints
+        vna.SCPI.SENSe(1).BANDwidth.RESolution = 20
+        vna.SCPI.CALCulate(1).PARameter(1).DEFine = "S21"
+        vna.SCPI.DISPlay.WINDow(1).TRACe(1).Y.SCALe.RPOSition = 10
+        vna.SCPI.CALCulate(1).SELected.FORMat = "MLOG"
+        vnaMdata = CType(vna.SCPI.CALCulate(1).SELected.DATA.FDATa, Double())
+        vnaFdata = CType(vna.SCPI.SENSe(1).FREQuency.DATA, Double())
+        vna.SCPI.CALCulate(1).SELected.MARKer(numPoints).ACTivate()
+
+    End Sub
+End Class
